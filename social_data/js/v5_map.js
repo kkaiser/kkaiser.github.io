98df121b2875e081460168f8dbe8007f2d6265f5
Kevin Kaiser <s151457@student.dtu.dk>
2016-05-16 19:46:24 +0200

    added clustering

--- social_data/js/v5_map.js
+++ social_data/js/v5_map.js
@@ -1,5 +1,5 @@
 "use strict";
-// create bounding box          
+// create bounding box
 var projection_map3 = d3.geo.mercator();
 //global variables
 var margin_map3 = {
@@ -43,8 +43,13 @@ var svg_map3 = d3.select("div#map3").append("svg")
 	.append("g")
 	.attr("transform", "translate(" + margin_map3.left + "," + margin_map3.top + ")");
 
+// define drawing groups, need to be in proper order (top element last)
+svg_map3.append("g").attr("id", "path_m3")
+svg_map3.append("g").attr("id", "nodes_m3")
+svg_map3.append("g").attr("id", "centroids_m3")
+
 d3.json("data/json/afg.geojson", function(json) {
-	svg_map3.selectAll("path")
+	svg_map3.select("#path_m3").selectAll("path")
 		.data(json.features)
 		.enter()
 		.append("path")
@@ -89,26 +94,70 @@ d3.json("data/json/afg.geojson", function(json) {
 				.style("display", "none")
 		});
 
-	d3.csv("data/csv/v5/geodata_kabul.csv", function(data) {
+	d3.csv("data/csv/v5/geodata_kabul.csv", function(error, data) {
+		if (error) throw error;
 
+		// node vars
 		var data_blue = [];
 		var data_red = [];
 		var data_green = [];
+		// centroid vars
+		var centr_e = [];
+		var centr_f = [];
+		var centr_n = [];
 		// Add a LatLng object to each item in the dataset
 		data.forEach(function(d) {
+			d.LatLng = new L.LatLng(d.lat, d.lon)
 			// append respective values
-			if (d.label == 1) {
-				data_red.push(d);
-			} else if (d.label == 2) {
+			if (d.label == 'friendly') {
 				data_green.push(d);
-			} else if (d.label == 3) {
+			} else if (d.label == 'enemy') {
+				data_red.push(d);
+			} else if (d.label == 'neutral') {
 				data_blue.push(d);
 			} else {
 				console.log("object not used: " + d)
 			}
 		});
 
-		svg_map3.selectAll("circle")
+		// made with: http://www.herethere.net/~samson/php/color_gradient/
+		function get_node_colors(d) {
+			if (d.label == 'friendly') {
+				if (d.cluster == 0) {
+					return "#90EE90";
+				} else if (d.cluster == 1) {
+					return "#60AF6A";
+				} else if (d.cluster == 2) {
+					return "#307045";
+				} else if (d.cluster == 3) {
+					return "#013220";
+				}
+			} else if (d.label == 'enemy') {
+				if (d.cluster == 0) {
+					return "#F88379";
+				} else if (d.cluster == 1) {
+					return "#DA625A";
+				} else if (d.cluster == 2) {
+					return "#BC413C";
+				} else if (d.cluster == 3) {
+					return "#9E201E";
+				} else if (d.cluster == 4) {
+					return "#800000";
+				}
+			} else if (d.label == 'neutral') {
+				if (d.cluster == 0) {
+					return "#89CFF0";
+				} else if (d.cluster == 1) {
+					return "#5B8AF5";
+				} else if (d.cluster == 2) {
+					return "#2D45FA";
+				} else if (d.cluster == 3) {
+					return "#0000FF";
+				}
+			}
+		}
+
+		svg_map3.select("#nodes_m3").selectAll("circle")
 			.data(data_green)
 			.enter()
 			.append("circle")
@@ -120,31 +169,60 @@ d3.json("data/json/afg.geojson", function(json) {
 			})
 			.attr("r", 3)
 			.style("fill", function(d) {
-				if (d.label == 1) {
-					return "#FF0000"
-				} else if (d.label == 2) {
-					return "#00FF00"
-				} else if (d.label == 3) {
-					return "#0000FF"
-				}
+				return get_node_colors(d);
 			})
 			.style("opacity", 0.45);
 
+		d3.json("data/json/centr.json", function(error, c_dat) {
+			if (error) throw error;
+
+			// append respective values
+			c_dat.enemy.forEach(function(d) {
+				centr_e.push(new L.LatLng(d[0], d[1]));
+			});
+			c_dat.friendly.forEach(function(d) {
+				centr_f.push(new L.LatLng(d[0], d[1]));
+			});
+			c_dat.neutral.forEach(function(d) {
+				centr_n.push(new L.LatLng(d[0], d[1]));
+			});
+
+
+			// centroids as white circle
+			svg_map3.select("#centroids_m3").selectAll("circle")
+				.data(centr_f)
+				.enter()
+				.append("circle")
+				.attr("cx", function(d) {
+					return projection_map3([d.lng, d.lat])[0];
+				})
+				.attr("cy", function(d) {
+					return projection_map3([d.lng, d.lat])[1];
+				})
+				.attr("r", 10)
+				.style("stroke-opacity", 0.6)
+				.style("stroke-width", 5)
+				.style("stroke", "white")
+				.style("fill", "none")
+				.style("opacity", 1);
+		})
+
+		// redraw each category every 8 seconds
 		var i = 0;
 		var inter = setInterval(function() {
 			i++;
 			if (i == 1) {
-				update_map(data_green);
+				update_map(data_green, centr_f);
 			} else if (i == 2) {
-				update_map(data_red);
+				update_map(data_red, centr_e);
 			} else if (i == 3) {
 				i = 0;
-				update_map(data_blue);
+				update_map(data_blue, centr_n);
 			}
-		}, 5000);
+		}, 8000);
 
-		function update_map(dat) {
-			svg_map3.selectAll("circle")
+		function update_map(dat, centr) {
+			svg_map3.select("#nodes_m3").selectAll("circle")
 				.data(dat)
 				.attr("cx", function(d) {
 					return projection_map3([d.lon, d.lat])[0];
@@ -154,15 +232,27 @@ d3.json("data/json/afg.geojson", function(json) {
 				})
 				.attr("r", 3)
 				.style("fill", function(d) {
-					if (d.label == 1) {
-						return "#FF0000"
-					} else if (d.label == 2) {
-						return "#00FF00"
-					} else if (d.label == 3) {
-						return "#0000FF"
-					}
+					return get_node_colors(d);
 				})
 				.style("opacity", 0.45);
+			
+			svg_map3.select("#centroids_m3").selectAll("circle").remove()
+			svg_map3.select("#centroids_m3").selectAll("circle")
+				.data(centr)
+				.enter()
+				.append("circle")
+				.attr("cx", function(d) {
+					return projection_map3([d.lng, d.lat])[0];
+				})
+				.attr("cy", function(d) {
+					return projection_map3([d.lng, d.lat])[1];
+				})
+				.attr("r", 10)
+				.style("stroke-opacity", 0.6)
+				.style("stroke-width", 5)
+				.style("stroke", "white")
+				.style("fill", "none")
+				.style("opacity", 1);
 		}
 	});
 });
